66a2b6e03965bb6eebb1ba2fe74caafeb6f883b0
Mark A. <mark001616@gmail.com>
2025-05-03 13:33:22 -0400

    Add support for dealing with req.body & JSON for REST API

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 // Router
 const authRouter = require("./routes/authRouter.js");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use("/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Launched on port: ${PORT}`));
