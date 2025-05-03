11fd14e2d4ea35df53cc6aa20252b963d099357d
Mark A. <mark001616@gmail.com>
2025-05-03 13:27:25 -0400

    Add more props to input component

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,11 +1,11 @@
 export default function Input(props) {
-  const { label } = props;
+  const { label, name, type } = props;
   return (
     <div className="p-2 flex flex-col gap-2">
       <label htmlFor={label} className="font-medium">
         {label}
       </label>
-      <input type="text" id={label} className="border rounded" />
+      <input type={type} name={name} id={label} className="border rounded" />
     </div>
   );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,13 +2,15 @@ import Input from "../components/Input";
 import NavLink from "../components/NavLink";
 
 export default function Login() {
+  const backendUrl = "http://localhost:3000";
+
   return (
     <div className="container flex flex-col gap-5">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold">Login</h1>
-        <form action="" className="flex-col">
-          <Input label="Username" />
-          <Input label="Password" />
+        <form action={backendUrl + "/auth"} method="post" className="flex-col">
+          <Input label="Username" name="username" type="text" />
+          <Input label="Password" name="password" type="password" />
           <button className="px-1 border rounded h-fit ">Log In</button>
         </form>
       </div>
