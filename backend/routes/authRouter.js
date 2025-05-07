4e745fe833adea1deb6deede420ad10c3caf25db
Mark A. <mark001616@gmail.com>
2025-05-07 07:17:45 -0400

    Fix damaged authRouter file

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -5,8 +5,7 @@ const authRouter = Router();
 const { loginPost, signupPost } = require("../controllers/authController.js");
 
 authRouter.post("/login", loginPost);
-authRouter.post("/signup", signupPost);
 
-// TODO: Implement logout
+authRouter.post("/signup", signupPost);
 
 module.exports = authRouter;
