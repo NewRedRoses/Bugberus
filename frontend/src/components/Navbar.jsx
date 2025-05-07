7a6ef10c1ff2ea2c3af388cd5cc0f56b3ea4e3ff
Mark A. <mark001616@gmail.com>
2025-05-07 06:18:45 -0400

    Generate prisma files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+    <div className="flex justify-end">
+      <button className="rounded-xl px-2 bg-slate-200 text-slate-800 font-bold hover:cursor-pointer">
+        Log Out
+      </button>
+    </div>
+  );
+}
