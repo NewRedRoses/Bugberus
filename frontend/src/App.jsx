fe9984456a90832c36c2948ee43cd4432c41fe4f
Mark A. <mark001616@gmail.com>
2025-05-07 21:53:34 -0400

    Add placeholder home dashboard with projects from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 import "./App.css";
+import NavBar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [projects, setProjects] = useState([]);
 
-  return <> Honey, I am home</>;
+  const projectsUrl = "http://localhost:3000/user/projects";
+  const token = localStorage.getItem("JWT");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(projectsUrl, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          setProjects(response.data);
+        }
+      })
+      .catch((error) => {
+        if (error.status == 403) {
+          localStorage.removeItem("JWT");
+          navigate("/login");
+        }
+      });
+  }, [projectsUrl]);
+
+  return (
+    <>
+      <NavBar />
+
+      <div className="container flex flex-col justify-center items-center">
+        <h1 className="pb-3 text-xl font-bold ">Projects</h1>
+        <ul className="flex gap-2 flex-wrap">
+          {projects.map((project, index) => {
+            return (
+              <li key={index} className="w-2xs p-2 border rounded-lg">
+                <div className="font-bold">{project.name}</div>
+                {project._count.bugs >= 1
+                  ? `${project._count.bugs} bug(s)`
+                  : project._count.bugs}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
