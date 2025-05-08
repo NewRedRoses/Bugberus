f92183456fe87bb30fee3d8dbe0b51a8a2e9f87d
Mark A. <mark001616@gmail.com>
2025-05-07 22:00:16 -0400

    Make pressing log out actually log out

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NavBar() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    localStorage.removeItem("JWT");
+    navigate("/login");
+  };
+
   return (
     <div className="flex justify-end">
-      <button className="rounded-xl px-2 bg-slate-200 text-slate-800 font-bold hover:cursor-pointer">
+      <button
+        className="rounded-xl px-2 bg-slate-200 text-slate-800 font-bold hover:cursor-pointer"
+        onClick={handleClick}
+      >
         Log Out
       </button>
     </div>
