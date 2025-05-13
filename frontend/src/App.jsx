9e4327dc688e443da1b29b350600d920a9884aeb
Mark A. <mark001616@gmail.com>
2025-05-13 14:49:08 -0400

    Add modal to create project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,14 @@ import "./App.css";
 import NavBar from "./components/Navbar";
 import { handlEnglisheWordingForMultiples } from "./helpers.js";
 import Button from "./components/Button.jsx";
+import Modal from "./components/Modal.jsx";
 
 function App() {
   const [projects, setProjects] = useState([]);
+  const [newProject, setNewProject] = useState({ name: "" });
 
   const projectsUrl = "http://localhost:3000/user/projects";
+  const createProjectUrl = "http://localhost:3000/project";
   const token = localStorage.getItem("JWT");
   const navigate = useNavigate();
 
@@ -32,25 +35,52 @@ function App() {
       });
   }, [projectsUrl]);
 
-  const handleAddProject = () => {
-    console.log("test from parent");
+  const handleAddProject = async () => {
+    await axios
+      .post(createProjectUrl, newProject, {
+        "Content-Type": "application/x-www-form-urlencoded",
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          // TODO: Change to proper notification later
+          alert("Post created successfully");
+          // Close modal
+        }
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
     <>
       <NavBar />
 
-      <div className="container flex flex-col justify-center items-center">
-        <span className="flex gap-4 align-center">
-          <h1 className="pb-3 text-xl font-bold ">Projects</h1>
-          <Button onClick={handleAddProject}>Add</Button>
+      <div className="container flex flex-col items-center justify-center">
+        <span className="align-center flex gap-4">
+          <h1 className="pb-3 text-xl font-bold">Projects</h1>
+
+          <Modal openBtnTitle="Add" modalTitle="Create a new project">
+            <div className="flex flex-col">
+              <label htmlFor="newProjectBtn">Project Name:</label>
+              <input
+                type=""
+                id="newProjectBtn"
+                className="mt-2 rounded border"
+                value={newProject.name}
+                onChange={(e) =>
+                  setNewProject({ ...newProject, name: e.target.value })
+                }
+              />
+            </div>
+            <Button onClick={handleAddProject}>Create</Button>
+          </Modal>
         </span>
-        <ul className="flex gap-2 flex-wrap gap-2">
+        <ul className="flex flex-wrap gap-2">
           {projects.map((project, index) => {
             return (
               <Link key={index} to={`/project/${project.id}`}>
-                <li className="w-2xs  p-2 shadow-md  rounded-2xl bg-yellow-100 text-yellow-900">
-                  <div className="font-bold pb-1">{project.name}</div>
+                <li className="w-2xs rounded-2xl bg-yellow-100 p-2 text-yellow-900 shadow-md">
+                  <div className="pb-1 font-bold">{project.name}</div>
                   {handlEnglisheWordingForMultiples("bug", project._count.bugs)}
                 </li>
               </Link>
