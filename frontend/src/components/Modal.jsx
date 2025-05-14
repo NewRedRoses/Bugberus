107d1bdd8edca98179d88aecc85a0477cbd1d9b8
Mark A. <mark001616@gmail.com>
2025-05-13 21:01:30 -0400

    Handle open/close logic from parent

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,45 +4,36 @@ import {
   DialogTitle,
   DialogBackdrop,
 } from "@headlessui/react";
-import { useState } from "react";
 
 // Inspo: https://headlessui.com/react/dialog
 
 export default function Modal({
   openBtnTitle = "Open",
-  openBtnClasses = "px-1 rounded border",
   modalTitle = "Title",
   modalTitleClasses,
+  isModalOpen,
+  setIsModalOpen,
   children = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, consectetur? ",
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsOpen(true);
-  };
-  const handleCloseModal = () => {
-    setIsOpen(false);
-  };
-
   return (
     <>
-      <button onClick={handleOpenModal} className={openBtnClasses}>
+      <button onClick={() => setIsModalOpen(true)} className={openBtnClasses}>
         {openBtnTitle}
       </button>
       <Dialog
-        open={isOpen}
-        onClose={handleCloseModal}
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
         className="relative z-50"
       >
         {/* For shadow backdrop  */}
         <DialogBackdrop className="fixed inset-0 bg-black/50" />
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-xl space-y-4 bg-slate-100 p-12 rounded">
+          <DialogPanel className="max-w-xl space-y-4 rounded bg-slate-100 p-12">
             <div className="flex justify-end pb-2">
               <button
                 id="close-modal-btn"
-                className="px-2 rounded bg-slate-300 text-slate-800 font-bold hover:cursor-pointer"
-                onClick={handleCloseModal}
+                className="rounded bg-slate-300 px-2 font-bold text-slate-800 hover:cursor-pointer"
+                onClick={() => setIsModalOpen(false)}
               >
                 Close
               </button>
@@ -51,9 +42,7 @@ export default function Modal({
             <DialogTitle className={`font-bold ${modalTitleClasses}`}>
               {modalTitle}
             </DialogTitle>
-            {typeof children === "function"
-              ? children({ closeModal: handleCloseModal })
-              : children}
+            {children}
           </DialogPanel>
         </div>
       </Dialog>
