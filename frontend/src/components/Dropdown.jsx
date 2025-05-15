dd7513cddb62ac7d1fedac1f9f16bb3ecb0970b5
Mark A. <mark001616@gmail.com>
2025-05-14 20:58:34 -0400

    Create semi-modular dropdown component

---
+++ frontend/src/components/Dropdown.jsx
@@ -0,0 +1,36 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+// https://headlessui.com/react/menu
+
+export default function Dropdown({
+  menuBtn = "Dropdown",
+  menuBtnClasses,
+  menuItems,
+  anchor = "bottom",
+  onClick,
+}) {
+  return (
+    <Menu>
+      <MenuButton className={menuBtnClasses}>{menuBtn}</MenuButton>
+      <MenuItems
+        anchor={anchor}
+        className="flex flex-col gap-2 rounded-md bg-slate-100 p-2 font-medium"
+      >
+        {menuItems.map((menuItem) => {
+          return (
+            <MenuItem
+              className={
+                menuItem.classNames ||
+                "px-2 hover:cursor-pointer data-focus:bg-slate-200"
+              }
+            >
+              <button type="" onClick={onClick}>
+                {menuItem.name}
+              </button>
+            </MenuItem>
+          );
+        })}
+      </MenuItems>
+    </Menu>
+  );
+}
