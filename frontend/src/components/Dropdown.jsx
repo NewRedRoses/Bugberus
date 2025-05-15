7b52c4a17f1cdbc616934e48033713a2a3fb4866
Mark A. <mark001616@gmail.com>
2025-05-14 21:15:52 -0400

    Fix missing key prop for list items

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -16,9 +16,10 @@ export default function Dropdown({
         anchor={anchor}
         className="flex flex-col gap-2 rounded-md bg-slate-100 p-2 font-medium"
       >
-        {menuItems.map((menuItem) => {
+        {menuItems.map((menuItem, index) => {
           return (
             <MenuItem
+              key={index}
               className={
                 menuItem.classNames ||
                 "px-2 hover:cursor-pointer data-focus:bg-slate-200"
