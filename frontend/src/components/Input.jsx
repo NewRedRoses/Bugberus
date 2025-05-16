42b945cfeca6adb445129e07f5c832b4e56b9efb
Mark A. <mark001616@gmail.com>
2025-05-16 16:05:31 -0400

    Modularize component to receive value and other props

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,13 @@
-export default function Input({ label, name, type, value, onChange }) {
+export default function Input({
+  label,
+  name,
+  type,
+  onChange,
+  value,
+  inputClassNames = "w-1/3 rounded border",
+}) {
   return (
-    <div className="flex flex-col gap-2 p-2">
+    <div className="flex flex-col gap-2">
       {label != undefined && (
         <label htmlFor={label} className="font-medium">
           {label}
@@ -10,7 +17,8 @@ export default function Input({ label, name, type, value, onChange }) {
         type={type}
         name={name}
         id={label}
-        className="w-1/3 rounded border"
+        value={value}
+        className={inputClassNames}
         onChange={onChange}
       />
     </div>
