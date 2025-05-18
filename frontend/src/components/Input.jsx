e0a3438b5afd43580167ef2c1accd89355e525f4
Mark A. <mark001616@gmail.com>
2025-05-18 03:01:09 -0400

    Configure input component with headless ui equiv

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,26 +1,66 @@
+import { Input as InputHeadless } from "@headlessui/react";
+
 export default function Input({
   label,
   name,
   type = "text",
   onChange,
   value,
-  inputClassNames = "rounded border",
+  className = "rounded border-2 border-slate-200 p-1",
 }) {
   return (
-    <div className="flex flex-col gap-2">
-      {label != undefined && (
-        <label htmlFor={label} className="font-medium">
-          {label}
-        </label>
-      )}
-      <input
-        type={type}
-        name={name}
-        id={label}
-        value={value}
-        className={inputClassNames}
-        onChange={onChange}
-      />
-    </div>
+    <InputHeadless
+      name={name}
+      type={type}
+      className={className}
+      value={value}
+      onChange={onChange}
+    />
   );
+}
+
+{
+  /* <div className="flex flex-col gap-2"> */
+}
+{
+  /*   {label != undefined && ( */
+}
+{
+  /*     <label htmlFor={label} className="font-medium"> */
+}
+{
+  /*       {label} */
+}
+{
+  /*     </label> */
+}
+{
+  /*   )} */
+}
+{
+  /*   <input */
+}
+{
+  /*     type={type} */
+}
+{
+  /*     name={name} */
+}
+{
+  /*     id={label} */
+}
+{
+  /*     value={value} */
+}
+{
+  /*     className={inputClassNames} */
+}
+{
+  /*     onChange={onChange} */
+}
+{
+  /*   /> */
+}
+{
+  /* </div> */
 }
