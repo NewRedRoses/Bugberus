0a97e8769a2da7a1ee20a641b54cbba02d0cff20
Mark A. <mark001616@gmail.com>
2025-05-17 20:53:29 -0400

    Add support for icons and other styling changes

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -7,26 +7,35 @@ export default function Dropdown({
   menuBtnClasses,
   menuItems,
   anchor = "bottom",
+  dropdownClasses = "bg-slate-200 border-2  border-slate-300 text-slate-700",
 }) {
   return (
     <Menu>
       <MenuButton className={menuBtnClasses}>{menuBtn}</MenuButton>
       <MenuItems
         anchor={anchor}
-        className="flex flex-col gap-2 rounded-md bg-slate-100 p-2 font-medium"
+        transition
+        className={`flex origin-top flex-col gap-2 rounded-xl p-3 font-semibold shadow-lg transition duration-200 ease-out data-closed:scale-95 data-closed:opacity-0 ${dropdownClasses}`}
+        as="div"
       >
-        {menuItems.map((menuItem, index) => {
+        {menuItems.map((menuItem) => {
+          const Icon = menuItem.icon;
+
           return (
             <MenuItem
-              key={index}
-              className={
-                menuItem.classNames ||
-                "px-2 hover:cursor-pointer data-focus:bg-slate-200"
-              }
+              key={menuItem.name}
+              className={`rounded px-2 ${menuItem.classNames}`}
             >
-              <button type="" onClick={menuItem.function}>
-                {menuItem.name}
-              </button>
+              <div className="flex items-center justify-start gap-2 data-focus:cursor-pointer">
+                {Icon && <Icon size={20} className="opacity-80" />}
+                <button
+                  type=""
+                  onClick={menuItem.function}
+                  className="hover:cursor-pointer"
+                >
+                  {menuItem.name}
+                </button>
+              </div>
             </MenuItem>
           );
         })}
