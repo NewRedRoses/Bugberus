c90686b53ace2310a512cf38146ca51065163a01
Mark A. <mark001616@gmail.com>
2025-05-17 22:57:09 -0400

    Redesign navigation links

--- frontend/src/components/NavLink.jsx
+++ frontend/src/components/NavLink.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router";
 export default function NavLink({
   to,
   children,
-  classNames = "h-fit px-2 font-bold text-slate-700 hover:cursor-pointer",
+  // classNames = "text-slate-700  flex justify-center align-center font-bold px-3  rounded underline-offset-4 hover:underline  hover:bg-slate-700 hover:text-slate-50 hover:no-underline",
+  classNames = "text-slate-700  flex justify-center align-center font-bold px-3 rounded underline-offset-2 hover:underline",
 }) {
   return (
     <Link to={to} className={classNames}>
