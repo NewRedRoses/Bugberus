7d131fc9ddcea895badecb2ff44d6c5096c221c1
Mark A. <mark001616@gmail.com>
2025-05-18 04:12:48 -0400

    Add default styling

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,24 @@
 export default function Button({
   children,
+  uiType = "normal",
   classNames = "h-fit rounded-md bg-slate-200 border-2 border-slate-300 px-2 font-bold text-slate-700 hover:cursor-pointer",
   onClick,
 }) {
-  return (
-    <button type="" className={classNames} onClick={onClick}>
-      {children}
-    </button>
-  );
+  switch (uiType) {
+    default:
+      return (
+        <button
+          className={`h-fit rounded-md border-2 border-slate-300 bg-slate-200 px-2 text-slate-700 hover:cursor-pointer ${classNames}`}
+          onClick={onClick}
+        >
+          {children}
+        </button>
+      );
+    case "custom":
+      return (
+        <button className={classNames} onClick={onClick}>
+          {children}
+        </button>
+      );
+  }
 }
