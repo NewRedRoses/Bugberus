1723cd67ca41f9122eb96b50db3fb2dedef17b61
Mark A. <mark001616@gmail.com>
2025-05-18 10:52:22 -0400

    Remove default prop values to be more flexiable

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,14 +1,9 @@
-export default function Button({
-  children,
-  uiType = "normal",
-  classNames = "h-fit rounded-md bg-slate-200 border-2 border-slate-300 px-2 font-bold text-slate-700 hover:cursor-pointer",
-  onClick,
-}) {
+export default function Button({ children, uiType, classNames, onClick }) {
   switch (uiType) {
     default:
       return (
         <button
-          className={`h-fit rounded-md border-2 border-slate-300 bg-slate-200 px-2 text-slate-700 hover:cursor-pointer ${classNames}`}
+          className={`h-fit rounded-md border-2 border-slate-300 bg-slate-200 px-2 font-bold text-slate-700 hover:cursor-pointer ${classNames}`}
           onClick={onClick}
         >
           {children}
