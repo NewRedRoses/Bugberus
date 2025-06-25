111755fd1ad503a97dc6e1ed31aacdd7e85120ca
Mark A. <mark001616@gmail.com>
2025-06-24 20:49:19 -0400

    Remove hardcoded title values and modularize classes

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -11,24 +11,27 @@ import Button from "./Button";
 // Inspo: https://headlessui.com/react/dialog
 
 export default function Modal({
-  openBtnTitle = "Open",
+  openBtnTitle,
   openBtnClasses,
   openBtnUiType,
-  modalTitle = "Title",
+  modalTitle,
   modalTitleClasses,
   isModalOpen,
   setIsModalOpen,
   children = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, consectetur? ",
+  modalCardClasses,
 }) {
   return (
     <>
-      <Button
-        onClick={() => setIsModalOpen(true)}
-        uiType={openBtnUiType}
-        classNames={openBtnClasses}
-      >
-        {openBtnTitle}
-      </Button>
+      {openBtnTitle && (
+        <Button
+          onClick={() => setIsModalOpen(true)}
+          uiType={openBtnUiType}
+          classNames={openBtnClasses}
+        >
+          {openBtnTitle}
+        </Button>
+      )}
       <Dialog
         open={isModalOpen}
         onClose={() => setIsModalOpen(false)}
@@ -37,7 +40,9 @@ export default function Modal({
         {/* For shadow backdrop  */}
         <DialogBackdrop className="fixed inset-0 bg-black/50" />
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-xl space-y-4 rounded bg-slate-100 p-10">
+          <DialogPanel
+            className={`max-w-xl space-y-4 rounded bg-slate-100 p-10 ${modalCardClasses}`}
+          >
             <div className="flex justify-end pb-2">
               <Button
                 classNames="text-red-800 hover:text-red-600 "
@@ -48,11 +53,13 @@ export default function Modal({
               </Button>
             </div>
 
-            <div className="mt-5">
-              <DialogTitle className={`font-bold ${modalTitleClasses}`}>
-                {modalTitle}
-              </DialogTitle>
-            </div>
+            {modalTitle && (
+              <div className="mt-5">
+                <DialogTitle className={`font-bold ${modalTitleClasses}`}>
+                  {modalTitle}
+                </DialogTitle>
+              </div>
+            )}
             {children}
           </DialogPanel>
         </div>
