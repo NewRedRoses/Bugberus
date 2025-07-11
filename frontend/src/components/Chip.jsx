96c96c3abfa4bef5825fc1856cb537864258624c
Mark A. <mark001616@gmail.com>
2025-07-10 21:51:29 -0400

    Support classes and onClick

--- frontend/src/components/Chip.jsx
+++ frontend/src/components/Chip.jsx
@@ -1,7 +1,14 @@
-export default function Chip({ children, bgColorClass, textColorClass }) {
+export default function Chip({
+  children,
+  bgColorClass,
+  textColorClass,
+  classNames,
+  onClick,
+}) {
   return (
     <div
-      className={`w-fit rounded-xl px-3 font-semibold ${bgColorClass} ${textColorClass}`}
+      className={`flex w-fit items-center rounded-xl px-3 font-semibold ${bgColorClass} ${textColorClass} ${classNames}`}
+      onClick={onClick}
     >
       {children}
     </div>
